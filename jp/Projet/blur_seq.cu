eb9b00971c57976e65ab2a70543f0ed3bef11d33
Jean-Pacôme Delmas <jeanpacome.delmas@hotmail.fr>
2020-04-19 15:12:36 +0200

    Update blur_seq.cu

--- jp/Projet/blur_seq.cu
+++ jp/Projet/blur_seq.cu
@@ -17,18 +17,109 @@ __global__ void grayscale( unsigned char * rgb, unsigned char * g, std::size_t c
     }
 }
 
-int sommeVoisins(const unsigned char * tab, int center, int cols) {
-//    cout << center << endl;
-    int hg = tab[center - 3 * cols - 1];
-//    int h = tab[center - 3 * cols];
-//    int hd = tab[center - 3 * cols + 1];
-    int g = tab[center - 1];
-    int d = tab[center + 1];
-//    int bg = tab[center + 3 * cols - 1];
-//    int b = tab[center + 3 * cols];
-//    int bd = tab[center + 3 * cols + 1];
-    return g + d;
-//    return hg + h + hd + g + d + bg + b + bd;
+unsigned char * sommePixelsVoisins(const unsigned char * rgb_in, int row, int col, int rows, int cols) {
+    auto * t =  new unsigned char[2];
+    unsigned char somme;
+    unsigned char div;
+    if (row * cols + col == 0) { // coin hg
+        unsigned char c = rgb_in[row * cols + col];
+        unsigned char d = rgb_in[row * cols + col + 3];
+        unsigned char b = rgb_in[(row + 3) * cols + col];
+        unsigned char bd = rgb_in[(row + 3) * cols + col + 3];
+        somme = c + d + b + bd;
+        div = '4';
+    }
+    else if (row * cols + col == cols - 1) { // coin hd
+        unsigned char g = rgb_in[row * cols + col - 3];
+        unsigned char c = rgb_in[row * cols + col];
+        unsigned char bg = rgb_in[(row + 3) * cols + col - 3];
+        unsigned char b = rgb_in[(row + 3) * cols + col];
+        somme = g + c + bg + b;
+        div = '4';
+    }
+    else if (row * cols + col == (rows - 1) * cols) { // coins bg
+        unsigned char h = rgb_in[(row - 3) * cols + col];
+        unsigned char hd = rgb_in[(row - 3) * cols + col + 3];
+        unsigned char c = rgb_in[row * cols + col];
+        unsigned char d = rgb_in[row * cols + col + 3];
+        somme = h + hd + c + d;
+        div = '4';
+    }
+    else if (row * cols + col == rows * cols - 1) { // coin bd
+        unsigned char hg = rgb_in[(row - 3) * cols + col - 3];
+        unsigned char h = rgb_in[(row - 3) * cols + col];
+        unsigned char g = rgb_in[row * cols + col - 3];
+        unsigned char c = rgb_in[row * cols + col];
+        unsigned char bd = rgb_in[(row + 3) * cols + col + 3];
+        somme = hg + h + g + c + bd;
+        div = '4';
+    }
+    else if (row * cols + col < cols) { // bordure h
+        unsigned char g = rgb_in[row * cols + col - 3];
+        unsigned char c = rgb_in[row * cols + col];
+        unsigned char d = rgb_in[row * cols + col + 3];
+        unsigned char bg = rgb_in[(row + 3) * cols + col - 3];
+        unsigned char b = rgb_in[(row + 3) * cols + col];
+        unsigned char bd = rgb_in[(row + 3) * cols + col + 3];
+        somme = g + c + d + bg + b + bd;
+        div = '6';
+    }
+    else if (row * cols + col > (rows - 1) * cols) { // bordure b
+        unsigned char hg = rgb_in[(row - 3) * cols + col - 3];
+        unsigned char h = rgb_in[(row - 3) * cols + col];
+        unsigned char hd = rgb_in[(row - 3) * cols + col + 3];
+        unsigned char g = rgb_in[row * cols + col - 3];
+        unsigned char c = rgb_in[row * cols + col];
+        unsigned char d = rgb_in[row * cols + col + 3];
+        somme = hg + h + hd + g + c + d;
+        div = '6';
+    }
+    else if (row * cols + col == row * cols) { // bordure g
+        unsigned char h = rgb_in[(row - 3) * cols + col];
+        unsigned char hd = rgb_in[(row - 3) * cols + col + 3];
+        unsigned char c = rgb_in[row * cols + col];
+        unsigned char d = rgb_in[row * cols + col + 3];
+        unsigned char b = rgb_in[(row + 3) * cols + col];
+        unsigned char bd = rgb_in[(row + 3) * cols + col + 3];
+        somme = h + hd + c + d + b + bd;
+        div = '6';
+    }
+    else if (row * cols + col == row * cols + cols - 1) { // bordure d
+        unsigned char hg = rgb_in[(row - 3) * cols + col - 3];
+        unsigned char h = rgb_in[(row - 3) * cols + col];
+        unsigned char g = rgb_in[row * cols + col - 3];
+        unsigned char c = rgb_in[row * cols + col];
+        unsigned char bg = rgb_in[(row + 3) * cols + col - 3];
+        unsigned char b = rgb_in[(row + 3) * cols + col];
+        somme = hg + h + g + c + bg + b;
+        div = '6';
+    }
+    else {
+        unsigned char hg = rgb_in[(row - 3) * cols + col - 3];
+        unsigned char h = rgb_in[(row - 3) * cols + col];
+        unsigned char hd = rgb_in[(row - 3) * cols + col + 3];
+        unsigned char g = rgb_in[row * cols + col - 3];
+        unsigned char c = rgb_in[row * cols + col];
+        unsigned char d = rgb_in[row * cols + col + 3];
+        unsigned char bg = rgb_in[(row + 3) * cols + col - 3];
+        unsigned char b = rgb_in[(row + 3) * cols + col];
+        unsigned char bd = rgb_in[(row + 3) * cols + col + 3];
+        somme = hg + h + hd + g + c + d + bg + b + bd;
+        div = '9';
+    }
+    t[0] = somme;
+    t[1] = div;
+    return t;
+}
+
+void blur(const unsigned char * rgb_in, unsigned char * rgb_out, int rows, int cols) {
+    for (int row = 0; row < rows; row++) {
+        for (int col = 0; col < cols; col++) {
+            unsigned char * sommeAndDiv = sommePixelsVoisins(rgb_in, row, col, rows, cols);
+            unsigned char newPixel = sommeAndDiv[0] / (int) sommeAndDiv[1];
+            rgb_out[row * cols + col] = newPixel;
+        }
+    }
 }
 
 int main()
@@ -37,28 +128,26 @@ int main()
     auto rgb = m_in.data;
     auto rows = m_in.rows;
     auto cols = m_in.cols;
-    cout << rows << endl;
-    cout << cols << endl;
-    cout << rgb << endl;
+    cout << "rows: " << rows << endl;
+    cout << "cols: " << cols << endl;
+    for (int i = 0; i < 100; i++) {
+        cout << rgb[i] << endl;
+    }
 
-//    std::vector< unsigned char > g( rows * cols );
+//    std::vector< unsigned char > g( rows * cols * 3 );
 //    cv::Mat m_out( rows, cols, CV_8UC1, g.data() );
-//
-//    unsigned char * rgb_d;
-//    unsigned char * g_d;
-//    cudaMalloc( &rgb_d, 3 * rows * cols );
-//    cudaMalloc( &g_d, rows * cols );
-//    cudaMemcpy( rgb_d, rgb, 3 * rows * cols, cudaMemcpyHostToDevice );
-//
-////    std::cout << sommeVoisins(rgb, ) << std::endl;
-//
-////    dim3 t( 32, 32 );
-////    dim3 b( ( cols - 1) / t.x + 1 , ( rows - 1 ) / t.y + 1 );
-////    grayscale<<< b, t >>>( rgb_d, g_d, cols, rows );
-//
-//    cudaMemcpy( g.data(), g_d, rows * cols, cudaMemcpyDeviceToHost );
+
+//    unsigned char * rgb_in;
+//    unsigned char * rgb_out;
+//    cudaMalloc( &rgb_in, 3 * rows * cols );
+//    cudaMalloc( &rgb_out, 3 * rows * cols );
+//    cudaMemcpy( rgb_in, rgb, 3 * rows * cols, cudaMemcpyHostToDevice );
+
+//    blur(rgb_in, rgb_out, rows, cols);
+
+//    cudaMemcpy(g.data(), rgb_out, 3 * rows * cols, cudaMemcpyDeviceToHost );
 //    cv::imwrite( "out.jpg", m_out );
-//    cudaFree( rgb_d);
-//    cudaFree( g_d);
+//    cudaFree(rgb_in);
+//    cudaFree(rgb_out);
     return 0;
 }
