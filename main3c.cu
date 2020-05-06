8a2677e7e76ad021b216307ef7ea7d8c55d0aa21
Nemense <guillaume.troesch@hotmail.fr>
2020-05-06 17:51:44 +0200

    debut test shared

--- main3c.cu
+++ main3c.cu
@@ -9,12 +9,13 @@ using namespace std;
 __global__ void copy(unsigned char * mat_in, unsigned char * mat_out, std::size_t cols, std::size_t rows) {
   auto i = blockIdx.x * blockDim.x + threadIdx.x;
   auto j = blockIdx.y * blockDim.y + threadIdx.y;
+  auto k = threadIdx.z;
 
   //if (i*cols+j < 3*cols*rows) // equivalent
   // if (blockIdx.x>0 &&  )
-  if (j>2 && j<rows*3-3 && i>2 && i<cols-3)
+  if (j>2 && j<rows*3-3 && i>2 && i+k<cols-3)
   {
-    mat_out[j * cols + i] = mat_in[j * cols + i];
+    mat_out[j * cols + i + k] = mat_in[j * cols + i + k];
 
   }
 }
@@ -46,7 +47,7 @@ __global__ void sharpen(unsigned char * mat_in, unsigned char * mat_out, std::si
   auto j = blockIdx.y * blockDim.y + threadIdx.y; //pos de la couleur sur y
 
   //if (j<rows*3 && i<cols && j>3 )
-  if (j>2 && j<rows*3-3)
+  if (j>2 && j<rows*3-3 && i<cols-3 && i>2)
   {
     //p1 à p9 correspondent aux 9 pixels à récupérer
     unsigned char p2 = mat_in[(j-3) * cols + i];
@@ -67,7 +68,7 @@ __global__ void edge_detect(unsigned char * mat_in, unsigned char * mat_out, std
   auto j = blockIdx.y * blockDim.y + threadIdx.y; //pos de la couleur sur y
 
   //if (j<rows*3 && i<cols && j>3 )
-  if (j>2 && j<rows*3-3)
+  if (j>2 && j<rows*3 && i<cols)
   {
     //p1 à p9 correspondent aux 9 pixels à récupérer
     unsigned char p2 = mat_in[(j-3) * cols + i];
@@ -84,25 +85,32 @@ __global__ void edge_detect(unsigned char * mat_in, unsigned char * mat_out, std
 }
 
 
-
 int main()
 {
   //Declarations
-  cv::Mat m_in = cv::imread("in.jpg", cv::IMREAD_UNCHANGED);
+  cv::Mat m_in = cv::imread("tulipe.jpg", cv::IMREAD_UNCHANGED);
   unsigned char * rgb = m_in.data;
   int rows = m_in.rows;
   int cols = m_in.cols;
+
+  // cv::Mat planes[3];
+  // cv::split(m_in, planes);
+  // unsigned char * b = planes[0].data;
+  // unsigned char * v = planes[1].data;
+  // unsigned char * r = planes[2].data;
+  // for (int i=0; i<rows; i++)
+  //   for (int j=0; j<cols; j++)
+  //     cout << r[j * cols + i];
+
 
   vector<unsigned char> g(rows * cols * 3); //Pour recreer l'image
   cv::Mat m_out(rows, cols, CV_8UC3, g.data());
   unsigned char * mat_in;
   unsigned char * mat_out;
-  unsigned char * mat_tmp;
 
   //Init donnes kernel
   cudaMalloc( &mat_in, 3 * rows * cols );
   cudaMalloc( &mat_out, 3 * rows * cols );
-  cudaMalloc( &mat_tmp, 3 * rows * cols );
   cudaMemcpy( mat_in, rgb, 3 * rows * cols, cudaMemcpyHostToDevice );
 
 
@@ -110,8 +118,8 @@ int main()
   // dim3 t( 32, 32 );
   // dim3 b( ( cols - 1) / (t.x-2) + 1 , ( rows - 1 ) / (t.y-2) + 1 );
 
-  dim3 block( 32, 32); //nb de thread, max 1024
-  dim3 grid((cols-1) / (block.x-2) + 1, 3*(rows-1) / (block.y-2) + 1);
+  dim3 block( 17, 20, 3); //nb de thread, max 1024
+  dim3 grid(((cols-1) / (block.x) + 1), ((rows-1) / block.y + 1)*3);
 
   //Debut de chrono
   cudaEvent_t start;
@@ -120,15 +128,11 @@ int main()
   cudaEventCreate(&stop);
   cudaEventRecord(start);
 
-  //Appel simple kernel
-  // copy<<< grid, block>>>(mat_in, mat_out, cols, rows);
-  blur<<< grid, block>>>(mat_in, mat_out, cols, rows);
+  //Appel kernel
+  copy<<< grid, block>>>(mat_in, mat_out, cols, rows);
+  // blur<<< grid, block>>>(mat_in, mat_out, cols, rows);
   // sharpen<<< grid, block>>>(mat_in, mat_out, cols, rows);
   // edge_detect<<< grid, block>>>(mat_in, mat_out, cols, rows);
-
-  //Double appel
-  // sharpen<<< grid, block>>>(mat_in, mat_tmp, cols, rows);
-  // edge_detect<<< grid, block>>>(mat_tmp, mat_out, cols, rows);
 
   //Fin de chrono
   cudaEventRecord(stop);
@@ -142,8 +146,6 @@ int main()
 
   //Recup donnees kernel
   cudaMemcpy( g.data(), mat_out, 3*rows * cols, cudaMemcpyDeviceToHost );
-
-
 
   cv::imwrite( "out.jpg", m_out );
 
