867332dbca43e7c27e555d9359b4cddc4ed281d9
Delmas Jean-Pacome <jean-pacome.delmas@etu.univ-orleans.fr>
2020-05-08 13:33:08 +0200

    test kernel blur

--- Code_test/kernel/main.cu
+++ Code_test/kernel/main.cu
@@ -8,7 +8,6 @@ __global__ void blur(const unsigned char* rgb_in, unsigned char* rgb_out, int ro
     if (row >= 1 && row < rows - 1 && col >= 1 && col < cols - 1)
     {
         for (int rgb = 0; rgb < 3; ++rgb) {
-
             unsigned char hg = rgb_in[3 * ((row - 1) * cols + col - 1) + rgb];
             unsigned char h = rgb_in[3 * ((row - 1) * cols + col) + rgb];
             unsigned char hd = rgb_in[3 * ((row - 1) * cols + col + 1) + rgb];
@@ -22,22 +21,6 @@ __global__ void blur(const unsigned char* rgb_in, unsigned char* rgb_out, int ro
             rgb_out[3 * (row * cols + col) + rgb] = (hg + h + hd + g + c + d + bg + b + bd) / 9;
         }
     }
-
-//    if (row >= 1 && row < rows - 1 && col >= 1 && col < cols - 1)
-//    {
-//        //p1 à p9 correspondent aux 9 pixels à récupérer
-//        unsigned char p1 = mat_in[(row - 3) * cols + col - 3];
-//        unsigned char p2 = mat_in[(row - 3) * cols + col];
-//        unsigned char p3 = mat_in[(row - 3) * cols + col + 3];
-//        unsigned char p4 = mat_in[row * cols + col - 3];
-//        unsigned char p5 = mat_in[row * cols + col];
-//        unsigned char p6 = mat_in[row * cols + col + 3];
-//        unsigned char p7 = mat_in[(row + 3) * cols + col - 3];
-//        unsigned char p8 = mat_in[(row + 3) * cols + col];
-//        unsigned char p9 = mat_in[(row + 3) * cols + col + 3];
-//
-//        mat_out[row * cols + col] = (p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9) / 9;
-//}
 }
 
 void main_blur(const dim3 grid, const dim3 block, const unsigned char* rgb_in, unsigned char* rgb_out_blur, int rows, int cols) {
@@ -85,6 +68,25 @@ int main()
     dim3 block(32, 32); //nb de thread, max 1024
     dim3 grid(((cols - 1) / block.x + 1), (rows - 1) / block.y + 1);
 
+    // Debut de chrono
+    cudaEvent_t start;
+    cudaEvent_t stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start);
+
+    // Appel kernel
+    blur <<< grid, block >>> (rgb_in, rgb_out_blur, rows, cols);
+
+    // Fin de chrono
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    std::cout << cudaGetErrorString(cudaGetLastError()) << std::endl;
+    float elapsedTime;
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    std::cout << "blur_kernel: " << elapsedTime << std::endl;
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
     /*// Debut de chrono
     cudaEvent_t start;
     cudaEvent_t stop;
@@ -105,7 +107,7 @@ int main()
     cudaEventDestroy(start);
     cudaEventDestroy(stop);*/
     // Execution
-    main_blur(grid, block, rgb_in, rgb_out_blur, rows, cols);
+//    main_blur(grid, block, rgb_in, rgb_out_blur, rows, cols);
 
     // Recup donnees kernel
     cudaMemcpy(g_blur.data(), rgb_out_blur, 3 * rows * cols, cudaMemcpyDeviceToHost);
